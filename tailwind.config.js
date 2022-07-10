4fa44540196d4e5ac800f0fbac30b215e303c3a3
petrkucerak <petr.kucera@mainstream.cz>
2022-07-10 14:33:12 +0200

    Create nextjs project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        athiti: ["Athiti", "sans-serif"],
+      },
+      colors: {
+        sunflower: "#FAD54D",
+      },
+    },
+  },
+  plugins: [],
+};
